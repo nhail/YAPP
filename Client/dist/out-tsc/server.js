0f19036f204a9dadf8148f01eacbc5db16fa3f43
Charles Rice <CharlesNRice@gmail.com>
2020-01-19 20:05:20 -0600

    Angular npm package checking

--- Client/dist/out-tsc/server.js
+++ Client/dist/out-tsc/server.js
@@ -1,10 +1,9 @@
 // THIS IS TO BOOTSTRAP LOADING AngularCLI from NodeJS.
 //  !!!! NOT FOR PRODUCTION USE !!!!
-import { spawn, SpawnOptions } from "child_process";
+import { spawn } from "child_process";
 import { join, resolve } from 'path';
 import { writeFileSync, readFileSync } from 'fs';
-
-//Read Azure Funcs settings file to create proxy setting file for AngularCLI
+//Read WebApi settings file to create proxy setting file for AngularCLI
 const currentDir = process.cwd();
 const apiSettingsPath = resolve(currentDir, "../Funcs/Properties/launchSettings.json");
 const apiSettingsContent = readFileSync(apiSettingsPath, { encoding: "utf8" }).replace(/^\uFEFF/, '');
@@ -13,20 +12,19 @@ let apiPort = "7071";
 if (apiSettings && apiSettings.profiles && apiSettings.profiles["nHail.YAPP.Funcs"] && apiSettings.profiles["nHail.YAPP.Funcs"].commandLineArgs) {
     const apiSettingsPort = apiSettings.profiles["nHail.YAPP.Funcs"].commandLineArgs
         .split("--").filter(function (x) {
-            return x.startsWith("port ");
-        })
+        return x.startsWith("port ");
+    })
         .reduce(function (acc, item) {
-            let detail = item.split(" ");
-            if (detail.length == 2) {
-                return detail[1];
-            }
-            return acc;
-        }, undefined);
+        let detail = item.split(" ");
+        if (detail.length == 2) {
+            return detail[1];
+        }
+        return acc;
+    }, undefined);
     if (apiSettingsPort) {
         apiPort = apiSettingsPort;
     }
 }
-
 const apiUrl = "http://localhost:" + apiPort + "/api";
 const tempProxySettingFile = join(currentDir, "WebApiProxy.json");
 const proxyData = {
@@ -36,16 +34,14 @@ const proxyData = {
     }
 };
 writeFileSync(tempProxySettingFile, JSON.stringify(proxyData));
-
-
 // launch Angular Cli as a NodeJs Process
-const options: SpawnOptions = {
+const options = {
     stdio: "inherit",
     shell: true
-}
+};
 const port = process.env.port || "4200";
 const angularCli = spawn("ng", ["serve", "--port " + port, "--proxy-config " + tempProxySettingFile, "--open"], options);
 angularCli.on("exit", (code, _) => {
     process.exit(code);
 });
-
+//# sourceMappingURL=server.js.map
